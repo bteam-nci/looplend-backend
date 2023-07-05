69aa0b5eeded21cbf5da1a0bf822f9c2cf13d02c
SirFrancis <borrellifrn@gmail.com>
2023-07-05 10:10:33 +0100

    fix on packaging

--- methods/libs/apighelper.js
+++ methods/libs/apighelper.js
@@ -5,7 +5,17 @@ module.exports.success = (body) => {
 		headers: {
 			"Access-Control-Allow-Origin": "*",
 			"Access-Control-Allow-Credentials": true,
-
 		}
 	}
 };
+
+module.exports.error = (body, statusCode) => {
+	return {
+		statusCode: statusCode ?? 500,
+		body: JSON.stringify(body),
+		headers: {
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Credentials": true,
+		}
+	}
+}
