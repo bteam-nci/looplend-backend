4c0986b1331643a33d87fd60902d77b98d5bc9a1
SirFrancis <borrellifrn@gmail.com>
2023-08-02 20:22:15 +0100

    fixes

--- methods/libs/repositories/products.js
+++ methods/libs/repositories/products.js
@@ -66,9 +66,7 @@ module.exports.create = async (product, dbInstance) => {
 
 module.exports.edit = async (product, dbInstance) => {
 	// create the product
-	const value = await dbInstance("products").insert(product).onConflict(["id"]).merge({
-		...product,
-	}).returning("*");
+	const value = await dbInstance("products").insert(product).onConflict(["id"]).merge().returning("*");
 	// insert the availabilities
 	const availabilities = product.availabilities.map((availability) => {
 		return {
