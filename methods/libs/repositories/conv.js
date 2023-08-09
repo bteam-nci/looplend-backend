df3a75ddabfaa8f08a80747b38e1bfd9590ce793
SirFrancis <borrellifrn@gmail.com>
2023-08-09 18:27:11 +0100

    fixes

--- methods/libs/repositories/conv.js
+++ methods/libs/repositories/conv.js
@@ -2,10 +2,10 @@ const PAGE_LIMIT = 25;
 
 module.exports.listMessages = async (params, dbInstance) => {
 	const {page, rID} = params;
-	const query = dbInstance("conversation_messages").where("rentalId", rID).innerJoin("users", "users.id", "conversation_messages.senderId").select("conversation_messages.*", "users.fullName as senderName");
+	const query = dbInstance("conversation_messages").where("rentalId", rID);
 	const total = await query.clone().count("*", {as: "total"}).first();
 
-	const messages = await query.clone().orderBy("createdAt", "desc").limit(PAGE_LIMIT).offset((page - 1) * PAGE_LIMIT);
+	const messages = await query.clone().innerJoin("users", "users.id", "conversation_messages.senderId").select("conversation_messages.*", "users.fullName as senderName").orderBy("createdAt", "desc").limit(PAGE_LIMIT).offset((page - 1) * PAGE_LIMIT);
 
 	return [messages.map(p => ({
 		value: p,
